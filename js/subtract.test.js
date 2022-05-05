375d395c55e12d97588b7121f274e2267c2d57aa
Zakaria Mubarik <101276169+zakariamubarik@users.noreply.github.com>
2022-05-05 10:52:24 +0000

    first commit

---
+++ js/subtract.test.js
@@ -0,0 +1,6 @@
+const subtract = require("./subtract")
+
+test("properly subtracting two numbers",() => {
+    expect(subtract(7, 2)
+    ).toBe(5)
+})
---
+++ js/sum.test.js
@@ -0,0 +1,6 @@
+const sum = require("./sum")
+
+test("properly adding two numbers",() => {
+    expect(sum(1, 2)
+    ).toBe(3)
+})
